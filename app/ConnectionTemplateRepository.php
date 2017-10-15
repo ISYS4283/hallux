289dd7c8bd5b5b9a2036dc7a5064f41569f0cf47
Jeff Puckett <jeff@jeffpuckett.com>
2017-10-15 03:06:45 +0000

    drop config scanner

--- app/ConnectionTemplateRepository.php
+++ app/ConnectionTemplateRepository.php
@@ -6,23 +6,51 @@ class ConnectionTemplateRepository
 {
     public function getTemplatesArray() : array
     {
-        $blacklist = [
-            'host',
-            'database',
-            'username',
-            'password',
+        return [
+            'sqlite' => [
+                'driver' => 'sqlite',
+                'database' => null,
+                'prefix' => null,
+            ],
+
+            'mysql' => [
+                'driver' => 'mysql',
+                'host' => '127.0.0.1',
+                'port' => '3306',
+                'database' => null,
+                'username' => null,
+                'password' => null,
+                'unix_socket' => null,
+                'charset' => 'utf8mb4',
+                'collation' => 'utf8mb4_unicode_ci',
+                'prefix' => '',
+                'strict' => true,
+                'engine' => null,
+            ],
+
+            'pgsql' => [
+                'driver' => 'pgsql',
+                'host' => '127.0.0.1',
+                'port' => '5432',
+                'database' => null,
+                'username' => null,
+                'password' => null,
+                'charset' => 'utf8',
+                'prefix' => '',
+                'schema' => 'public',
+                'sslmode' => 'prefer',
+            ],
+
+            'sqlsrv' => [
+                'driver' => 'sqlsrv',
+                'host' => '127.0.0.1',
+                'port' => '1433',
+                'database' => null,
+                'username' => null,
+                'password' => null,
+                'charset' => 'utf8',
+                'prefix' => '',
+            ],
         ];
-
-        foreach (config('database.connections') as $key => $connection) {
-            foreach ($connection as $name => &$attribute) {
-                if (in_array($name, $blacklist)) {
-                    $attribute = null;
-                }
-            }
-
-            $return[$key] = $connection;
-        }
-
-        return $return;
     }
 }
