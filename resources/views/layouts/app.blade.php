1b1b4e77ff1000899ca4a5d3fcc78eae0ce8585a
Jeff Puckett <jeff@jeffpuckett.com>
2017-10-16 20:40:58 +0000

    add shibboleth

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -14,6 +14,14 @@
     <li><a href="{{ route('connections.index') }}">Connections</a></li>
 @endsection
 
+@section('navbar-right')
+    @auth
+        <li><a href="/shibboleth-logout">Logout {{ Auth::user()->name }}</a></li>
+    @else
+        <li><a href="/shibboleth-login">Login</a></li>
+    @endauth
+@endsection
+
 @section('scripts')
     <script src="https://cdn.rawgit.com/google/code-prettify/master/loader/run_prettify.js?lang=sql&skin=sons-of-obsidian"></script>
 @endsection
