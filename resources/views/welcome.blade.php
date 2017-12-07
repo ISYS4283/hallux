6d7eb4185bee98f44d7f3886e5eadcfa0bfb01c6
Jeff Puckett <jeff@jeffpuckett.com>
2017-12-07 03:34:50 +0000

    doc project purpose and overview

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -10,7 +10,23 @@
     @include('flash::message')
 
     <p class="lead">
-        This is an SQL exploration environment for learning how to query a
-        database for answers to common business questions.
+        SQL testing environment for learning how to query a database
+    </p>
+
+    <p>
+        Herein you will find business questions, and their result sets.
+        Your objective is to write SQL queries that produce the given result sets.
+    </p>
+
+    <p>
+        It will probably be more efficient and faster to work on your solutions
+        in a dedicated querying environment such as SQL Server Management Studio.
+        Once you have a working solution, then check your work by submitting here.
+    </p>
+
+    <p>
+        Some quizzes are integrated with Blackboard.
+        To get credit, be sure to submit your work when you see
+        <button type="button" class="btn btn-primary">Post to Blackboard</button>
     </p>
 @endsection
